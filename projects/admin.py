d40211d3701dbb567aec1f1cd97a9a89806b2629
Ibby360 <ibrahimramadhan360@outlook.com>
2021-09-18 16:01:09 +0300

    Add project model on the admin site

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,16 @@
+from projects.models import Projects, Projectimages
 from django.contrib import admin
-
-# Register your models here.
+ 
+class ProjectsImageAdmin(admin.StackedInline):
+    model = Projectimages
+ 
+@admin.register(Projects)
+class ProjectsAdmin(admin.ModelAdmin):
+    inlines = [ProjectsImageAdmin]
+ 
+    class Meta:
+       model = Projects
+ 
+@admin.register(Projectimages)
+class ProjectsImageAdmin(admin.ModelAdmin):
+    pass
