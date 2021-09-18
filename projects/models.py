b5ddaa77df9939011acf7d7920ad28d4fcef1fdb
Ibby360 <ibrahimramadhan360@outlook.com>
2021-09-18 16:02:53 +0300

    ✨ Projects Model

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.db.models.fields import TextField
+from taggit.managers import TaggableManager
 
 # Create your models here.
+
+class Projects(models.Model):
+    title = models.CharField(max_length=150)
+    description = models.TextField()
+    tags = TaggableManager()
+
+    class Meta:
+        verbose_name = 'Project'
+        verbose_name_plural = 'Projects'
+
+    def __str__(self):
+        return self.title
+
+class Projectimages(models.Model):
+    project = models.ForeignKey(Projects, on_delete=models.CASCADE)
+    images = models.FileField(upload_to='project/images/')
+
+    class Meta:
+        verbose_name = 'Project Image'
+        verbose_name_plural = 'Project Images'
+
+    def __str__(self):
+        return self.project.title
+    
