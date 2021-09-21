1330a6b323f0e8e48288a5518bf64720e6cac407
Ibby360 <ibrahimramadhan360@outlook.com>
2021-09-21 16:12:48 +0300

    ✨ Create home view

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from projects.models import Projects
+from django.shortcuts import render, get_object_or_404
+from projects.models import Projects, Projectimages
 
 # Create your views here.
+def index(request):
+    projects = Projects.objects.all()
+    context = {
+        'projects': projects
+    }
+    return render(request, 'index.html', context)
