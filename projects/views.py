fd21a38d3af8f2e69b580ec7b64ba95358b75a40
Ibby360 <ibrahimramadhan360@outlook.com>
2021-09-24 14:58:23 +0300

    Post list object

--- projects/views.py
+++ projects/views.py
@@ -1,11 +1,14 @@
 from projects.models import Projects
 from django.shortcuts import render, get_object_or_404
 from projects.models import Projects, Projectimages
+from blog.models import Post
 
 # Create your views here.
 def index(request):
     projects = Projects.objects.all()
+    posts = Post.objects.filter(status='published').order_by('-publish')
     context = {
-        'projects': projects
+        'projects': projects,
+        'posts': posts,
     }
     return render(request, 'index.html', context)
