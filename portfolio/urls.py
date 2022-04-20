db5d4628b35f271704a30acec43853ab48bdf7fb
Ibby360 <ibrahimramadhan360@outlook.com>
2022-04-20 06:09:43 +0300

    Create project model

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -6,5 +6,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('projects.urls')),
-    path('blog', include('blog.urls')),
 ] 
--- projects/models.py
+++ projects/models.py
@@ -10,6 +10,7 @@ class Projects(models.Model):
     image = models.ImageField(upload_to='project/image', blank=True, null=True )
     project_link = models.CharField(max_length=250, null=True, blank=True)
     github_repo = models.CharField(max_length=250, null=True, blank=True)
+    is_featured = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
